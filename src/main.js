12ba5ca838967ad662fca07d078ee414ef44a547
Jan Robert <janmartinrobert@gmail.com>
2020-02-07 17:23:36 +0100

    Add link to wikipedia and start button

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Cascade from './Cascade'
 
 const p = document.querySelector('p')
-
-const cascade = new Cascade(p)
-cascade.run()
+const btn = document.querySelector('button')
+btn.addEventListener(
+	'click',
+	() => {
+		const cascade = new Cascade(p)
+		cascade.run()
+		btn.disabled = true
+	},
+	{ once: true }
+)
